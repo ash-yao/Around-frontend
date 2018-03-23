93dd3db26610cd6716f4e1dab32294969903425d
ash-yao <ash210104@gmail.com>
2018-03-22 22:23:03 -0500

    add header text

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ export class Header extends React.Component {
     render() {
         return (<header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className={"App-title"}>Around</h1>
         </header>)
     }
 }
