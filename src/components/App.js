d9afac1cc5e42404e53f1c7c5c8842b5e0fcec5b
ash-yao <ash210104@gmail.com>
2018-03-24 16:43:44 -0500

    login step one

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Header } from './Header'
 import { Main } from './Main'
 import '../styles/App.css';
-import '../styles/Main.css';
-import '../styles/Register.css';
 
 class App extends Component {
   render() {
